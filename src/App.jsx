be9f812b217c60c4b86a798cac0210c7ef5511b6
condejoaco <condejoaco@gmail.com>
2023-08-15 22:25:40 -0300

    Modelo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Card from "./Card";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
